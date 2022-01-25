9f52b8c895e6b19b4888b4a20faed4bb174aa5c1
Raj sen <97875723+errorraaz@users.noreply.github.com>
2022-01-26 00:03:33 +0530

    Update markstopercantage.java

--- markstopercantage.java
+++ markstopercantage.java
@@ -8,15 +8,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to CBSC Borad Exam marks to percantage calculater"); // Title of the program
         Scanner s = new Scanner(System.in); // Defining the scanner for user input
-        System.out.println("Enter maths number"); // input hint for user
+        
+        System.out.println("Enter Maths number"); // input hint for user
         int a = s.nextInt(); // User input first value
-        System.out.println("Enter english number"); // input hint for user
+        System.out.println("Enter English number"); // input hint for user
         int b = s.nextInt(); // User input first value
-        System.out.println("Enter hindi number"); // input hint for user
+        System.out.println("Enter Hindi number"); // input hint for user
         int c = s.nextInt(); // User input first value
-        System.out.println("Enter science number"); // input hint for user
+        System.out.println("Enter Science number"); // input hint for user
         int d = s.nextInt(); // User input first value
-        System.out.println("Enter social science number"); // input hint for user
+        System.out.println("Enter Social science number"); // input hint for user
         int e = s.nextInt(); // User input first value
 
         // Calculating the value of all students and program give it to percentage of student using our students marks
